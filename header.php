f43104f446122b9765deff67630adec87beb88c8
Malin Persson <malin.persson02@hotmail.com>
2025-11-12 19:47:26 +0100

    Added assets and started on header and index page

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kinforma</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=B612:ital,wght@0,400;0,700;1,400;1,700&family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Rubik:ital,wght@0,700;1,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <div class="header-wrapper">
+            <div class="logo-container">
+                <img class="logo" src="assets/logo.svg">
+                <h3 class="rubik-logo">Kinforma</h3>
+            </div>
+            <div class="nav-container">
+                <img class="cart" src="assets/shopping cart.svg">
+                <img class="nav-button" src="assets/nav.svg">
+            </div>
+        </div>
+
+    </header>
---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+require_once __DIR__ . '/header.php';
+?>
+
+<main>
+    <section class="hero">
+        <div class="hero-container">
+            <img class="hero-img" src="assets/hero.png">
+        </div>
+        <div class="text-container">
+            <p class="button">EXPORE THE COLLECTION</p>
+        </div>
+    </section>
+    <section class="scroll">
+        <div class="section-head">
+            <h3>DROP 01: ADAPTIVE FORMS</h3>
+            <div class="arrow-control">
+            </div>
+            <img class="arrows" src="assets/arrow left.svg">
+            <img class="arrows" src="assets/arrow right.svg">
+        </div>
+        <div class="product-scroll">
+            <div class="product-container">
+                <div class="product-img-container">
+                    <img class="product" src="assets/product.jpg">
+                </div>
+                <div class="expand-product">
+                    <h4 class="product-name">SHELL JACKET</h4>
+                    <img class="plus" src="assets/plus.svg">
+                </div>
+                <div class="color-picker">
+
+                </div>
+            </div>
+    </section>
+</main>
+
+<?php
+require_once __DIR__ . '/footer.php';
+?>
