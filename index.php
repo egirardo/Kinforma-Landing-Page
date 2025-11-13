3dc40e79ec1784586ca21cf089bbd629ee9a16f7
Malin Persson <malin.persson02@hotmail.com>
2025-11-13 16:15:01 +0100

    Finished the main skeleton

--- index.php
+++ index.php
@@ -38,15 +38,24 @@ require_once __DIR__ . '/header.php';
     </section>
 
     <section class="ai-design">
-        <h3>ADAPTIVE DESIGN, BUILT FOR YOUR NEEDS.</h3>
+        <h4>ADAPTIVE DESIGN, BUILT FOR YOUR NEEDS.</h4>
         <p class="button">CUSTOMIZE YOUR GEAR</p>
     </section>
 
     <section class="tech">
-        <h3>EXPLORE THE LAYERS BEHIND THE INNOVATION.</h3>
+        <h4>EXPLORE THE LAYERS BEHIND THE INNOVATION.</h4>
         <p>Our fibers are crafted with regenerative technolagy for a sustaniable future.</p>
         <div class="tech-img-container">
             <img class="tech-img" src="assets/layers.jpg">
+        </div>
+        <p class="button">LEARN ABOUT OUR SUSTAINABILITY</p>
+    </section>
+
+    <section class="function">
+        <h4>FORM MEETS FUNCTION</h4>
+        <p>Every line, seam and fiber is designed to last and evolve with you.</p>
+        <div class="motion-img-container">
+            <img class="motion-img" src="assets/motion.jpg">
         </div>
     </section>
 </main>
