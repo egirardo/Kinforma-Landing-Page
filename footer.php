22bf81b461670d7a45c9d1b85dd5cfdda25cf7cd
Elsa Girardo <egirardo@hotmail.com>
2025-11-13 22:42:13 +0100

    Feature: added footer content and styling. Also added assets to assets folder.

--- footer.php
+++ footer.php
@@ -1,5 +1,33 @@
 </body>
 <footer>
+    <b class="kontakt">Kontakt</b>
+    <b class="support">support</b>
+    <b class="fretag">Företag</b>
+    <b class="flj-oss">Följ oss</b>
+    <b class="rttslig">Rättslig</b>
+    <b class="handla-i">HANDLA I:</b>
+    <b class="sprk">SPRÅK</b>
+    <b class="nyhetsbrev">NYHETSBREV</b>
+    <div class="e-postadress">
+        <span>E-POSTADRESS </span>
+        <span class="span">*</span>
+    </div>
+    <div class="sverige">SVERIGE</div>
+    <div class="svenska">SVENSKA</div>
+    <div class="upphovsrtt-2025-luxurywear-container">
+        <span class="upphovsrtt">UPPHOVSRÄTT &COPY;2025 LUXURYWEAR COMPANY SPA <br>ALLA RÄTTIGHETER FÖRBEHÅLLNA. - <br></span>
+        <b class="fretagsinformation">FÖRETAGSINFORMATION</b>
+    </div>
+    <div class="ange-din-e-postadress">Ange din e-postadress</div>
+    <div class="footer-stoneisland-item"></div>
+    <div class="fortstta">FORTSÄTTA</div>
+    <img class="mageplus-icon" src="/assets/mage_plus.svg" alt="plus sign">
+    <img class="footer-stoneisland-mageplus-icon" src="/assets/mage_plus.svg" alt="plus sign">
+    <img class="mageplus-icon2" src="/assets/mage_plus.svg" alt="plus sign">
+    <img class="mageplus-icon3" src="/assets/mage_plus.svg" alt="plus sign">
+    <img class="mageplus-icon4" src="/assets/mage_plus.svg" alt="plus sign">
+    <img class="mageplus-icon5" src="/assets/mage_plus.svg" alt="plus sign">
+    <img class="mageplus-icon6" src="/assets/mage_plus.svg" alt="plus sign">
 
 </footer>
 
