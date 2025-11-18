5910bc8b824f6ab5d5cf4c080b42b113dc5d2fb5
Elsa Girardo <egirardo@hotmail.com>
2025-11-18 15:44:33 +0100

    Feature: restructured footer menu and added input form for newsletter. styling for newsletter still in progress

--- footer.php
+++ footer.php
@@ -1,33 +1,47 @@
 </body>
 <footer>
-    <b class="kontakt">Kontakt</b>
-    <b class="support">support</b>
-    <b class="fretag">Företag</b>
-    <b class="flj-oss">Följ oss</b>
-    <b class="rttslig">Rättslig</b>
+
+    <div class="footer-container">
+        <div class="footer-menu-container">
+            <div class="footer-menu-item">
+                <h5>CONTACT</h5>
+                <img class="plus-icon" src="/assets/mage_plus.svg" alt="plus sign">
+            </div>
+            <div class="footer-menu-item">
+                <h5>SUPPORT</h5>
+                <img class="plus-icon" src="/assets/mage_plus.svg" alt="plus sign">
+            </div>
+            <div class="footer-menu-item">
+                <h5>COMPANY</h5>
+                <img class="plus-icon" src="/assets/mage_plus.svg" alt="plus sign">
+            </div>
+            <div class="footer-menu-item">
+                <h5>FOLLOW US</h5>
+                <img class="plus-icon" src="/assets/mage_plus.svg" alt="plus sign">
+            </div>
+            <div class="footer-menu-item">
+                <h5>LEGAL</h5>
+                <img class="plus-icon" src="/assets/mage_plus.svg" alt="plus sign">
+            </div>
+        </div>
+        <div class="newsletter-container">
+            <h3 class="newsletter-title">NEWSLETTER</h3>
+            <form action="/index.php" method="post" enctype="text/plain">
+                <label for="email">EMAIL ADDRESS *</label>
+                <input type="email" name="email" id="email" placeholder="Enter your email address">
+                <button class="button" type="submit">SIGN UP</button>
+            </form>
+        </div>
+    </div>
     <b class="handla-i">HANDLA I:</b>
     <b class="sprk">SPRÅK</b>
-    <b class="nyhetsbrev">NYHETSBREV</b>
-    <div class="e-postadress">
-        <span>E-POSTADRESS </span>
-        <span class="span">*</span>
-    </div>
     <div class="sverige">SVERIGE</div>
     <div class="svenska">SVENSKA</div>
     <div class="upphovsrtt-2025-luxurywear-container">
         <span class="upphovsrtt">UPPHOVSRÄTT &COPY;2025 LUXURYWEAR COMPANY SPA <br>ALLA RÄTTIGHETER FÖRBEHÅLLNA. - <br></span>
         <b class="fretagsinformation">FÖRETAGSINFORMATION</b>
     </div>
-    <div class="ange-din-e-postadress">Ange din e-postadress</div>
-    <div class="footer-stoneisland-item"></div>
-    <div class="fortstta">FORTSÄTTA</div>
-    <img class="mageplus-icon" src="/assets/mage_plus.svg" alt="plus sign">
-    <img class="footer-stoneisland-mageplus-icon" src="/assets/mage_plus.svg" alt="plus sign">
-    <img class="mageplus-icon2" src="/assets/mage_plus.svg" alt="plus sign">
-    <img class="mageplus-icon3" src="/assets/mage_plus.svg" alt="plus sign">
-    <img class="mageplus-icon4" src="/assets/mage_plus.svg" alt="plus sign">
-    <img class="mageplus-icon5" src="/assets/mage_plus.svg" alt="plus sign">
-    <img class="mageplus-icon6" src="/assets/mage_plus.svg" alt="plus sign">
+
 
 </footer>
 
