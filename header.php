8045eb12cbe0f9f797e85f3acc4b0950c58b6c12
Elsa Girardo <egirardo@hotmail.com>
2025-11-22 10:46:36 +0100

    Feature: Added large media css file

--- header.php
+++ header.php
@@ -19,6 +19,7 @@ require __DIR__ . "/assets.php";
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="large-media.css">
     <link rel="stylesheet" href="fonts.css">
 </head>
 
